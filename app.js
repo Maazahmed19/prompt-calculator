9c6bda65e77a7ef90c286e9b8b9f56e6b8d19462
Maazahmed19 <154379238+Maazahmed19@users.noreply.github.com>
2024-03-14 09:50:12 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+let num1 = prompt("enter first number")
+let num2 = prompt("enter second number")
+num1 = Number(num1);
+num2 = Number(num2);
+console.log ("num1,num2")
+console.log ("num2,num1") 
+let sum =num1 + num2;
+let minus =num1 - num2;
+alert("addition"+sum);
+alert("substruction"+minus);
